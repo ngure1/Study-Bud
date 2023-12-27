a956879a75e89e6583bad3a00015ca2bca096b7f
Joseph Ngure <ngugijoseph700@gmail.com>
2023-12-27 11:44:38 +0300

    Login functionality added

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render,redirect
 from . import models
 from . import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 from django.db.models import Q
 
-
-
+def loginView(request):
+  if request.method =='POST':
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    try:
+      user=User.objects.get(username=username)
+    except:
+      messages.error(request,"User does not exist")
+      # return redirect('create accout')
+    user=authenticate(request,username=username,password=password)
+    if user is not None:
+      login(request,user)
+      return redirect('home')
+    else:
+      messages.error(request,"Username or password does not exist")
+  context={}
+  return render(request , 'base/login.html', context)
+def logoutView(request):
+  logout(request)
+  return redirect ('home')
 def home(request):
   q=request.GET.get('q') if request.GET.get('q') != None else ''
   rooms=models.Rooms.objects.filter(
